3cc0558515ae81222e7b7dc479e2ffd084d6506e
Inja-m <inja@mainhardts.de>
2025-05-28 16:38:34 +0200

    only backend

--- frontend/middleware/auth.global.ts
+++
@@ -1,9 +0,0 @@
-export default defineNuxtRouteMiddleware(async (to) => {
-  // Seite /login überspringen
-  if (to.path === '/login') return
-	
-  const token = useStrapiToken()
-  if (!token.value) {
-    return navigateTo('/login')
-  }
-})
--- frontend/utils/parseMarkdown.ts
+++
@@ -1,5 +0,0 @@
-import { marked } from 'marked'
-
-export function parseMarkdown(input: string): string {
-  return marked.parse(input || '')
-}
