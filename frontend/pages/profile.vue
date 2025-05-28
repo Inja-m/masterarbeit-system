0428890e8224d11800158d12ab03498adf979705
Inja-m <inja@mainhardts.de>
2025-05-28 09:50:16 +0200

    Profile Minimum

--- frontend/pages/profile.vue
+++ frontend/pages/profile.vue
@@ -1,24 +1,27 @@
 <template>
-	<Section>
-		  <div class="profile-container">
-    <div class="profile-header">
-      <h1 class="text-center text-2xl font-semibold">Mein Profil</h1>
+  <Section>
+    <div class="flex flex-col items-center justify-center space-y-6 py-12">
+      <UAvatar :alt="user.username" :ui="{      root: 'size-36 text-4xl'   }" />
+      <h1 class="text-2xl font-semibold">{{ user.username }}</h1>
+      <UButton 
+        trailing-icon="i-lucide-log-out" 
+        size="md" 
+        color="neutral"
+        class="mt-4" 
+				@click="handleLogout" 
+      >
+        Ausloggen
+      </UButton>
     </div>
-{{ user }}
-<!--<UAvatar :alt="user.username" size="3xl" />
-{{ user.username }}-->
-
-<UButton @click="handleLogout" trailing-icon="i-lucide-log-out" size="md">Ausloggen</UButton>
-  </div>
-	</Section>
+  </Section>
 </template>
 
 <script setup lang="ts">
 const { logout, fetchUser } = useStrapiAuth()
 const user = await fetchUser()
+
 const handleLogout = async () => {
   await logout()
   navigateTo('/login')
 }
 </script>
-
