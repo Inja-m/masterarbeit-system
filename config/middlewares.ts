2870e353ec4ad9c43c5ea3394176206778145ed6
Inja-m <inja@mainhardts.de>
2025-05-29 11:15:43 +0200

    add cors and security setting

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,8 +1,29 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+	{
+		name: 'strapi::security',
+		config: {
+			contentSecurityPolicy: {
+				useDefaults: true,
+				directives: {
+					"connect-src": ["'self'", process.env.FRONTEND_URL], 
+					"img-src": ["'self'", "data:", "blob:"],
+					"media-src": ["'self'", "data:", "blob:"],
+					upgradeInsecureRequests: null,
+				},
+			},
+		},
+	},
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [process.env.FRONTEND_URL], // dein Frontend (kein / am Ende)
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization'],
+      credentials: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
