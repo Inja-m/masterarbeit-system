1628a15d557534060e0507f0c4bc9572e0a939d5
Inja-m <inja@mainhardts.de>
2025-07-24 19:21:48 +0200

    mail provider

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,30 @@
-export default () => ({});
+
+export default ({ env }) => ({
+  // ...
+  'users-permissions': {
+    config: {
+      jwt: {
+        expiresIn: '30d',
+      },
+    },
+  },
+	email: {
+    config: {
+      provider: 'nodemailer',
+			providerOptions:{
+				host: env('SMTP_HOST'),
+				port:env.int('SMTP_PORT'),
+				secure: true,
+				auth:{
+					user: env('SMTP_USERNAME'),
+					pass: env('SMTP_PASSWORD')
+				}
+			},
+      settings: {
+        defaultFrom: env('SMTP_USERNAME'),
+        defaultReplyTo: env('SMTP_USERNAME')
+      },
+    },
+  },
+  // ...
+});
